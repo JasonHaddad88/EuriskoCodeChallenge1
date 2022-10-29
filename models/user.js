dc5d34cd430760bd081058c0dfa88dbb75f81a45
JasonHaddad88 <116954774+JasonHaddad88@users.noreply.github.com>
2022-10-29 15:13:58 +0300

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true
+        },
+
+        email: {
+            type: String,
+            required: true
+        },
+
+        password: {
+            type: String,
+            required: true
+        },
+
+        notes: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Note'
+            }
+        ]
+
+    }
+)
+
+module.exports = mongoose.model('User', userSchema)
